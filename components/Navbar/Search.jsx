84fff7ad380e073052b6730ef00beca32dc2569e
NieamulKabir <kabir000079@gmail.com>
2024-05-05 22:49:41 +0600

    debounce-hook add

--- components/Navbar/Search.jsx
+++ components/Navbar/Search.jsx
@@ -1,6 +1,12 @@
+'use client'
+
+import { useDebounce } from "@/hooks/useDebounce";
+import { useState } from "react";
 
 
 const Search = () => {
+    const [searchResult,setSearchResult]=useState([])
+    const [term,setTerm]=useState("")
     return (
         <div>
             search
